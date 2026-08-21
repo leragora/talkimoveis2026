0dee9298241691f29532a0899fe05796fc05e6f8
Geraldo Neto <geraldo.linkedit@gmail.com>
2026-08-20 21:55:51 -0300

    rev02

--- assets/js/in5.config.js
+++ assets/js/in5.config.js
@@ -951,7 +951,7 @@ function getOrientation() {
 }
 
 function addNavProps(){
-	if(nav.numPages === undefined) nav.numPages=79;
+	if(nav.numPages === undefined) nav.numPages=80;
 	nav.rtl = $('#slider').attr('data-dir') == 'rtl';
 	if(nav.rtl) $('html').attr('data-dir', 'rtl');
 	nav.init = function() { setTimeout(function(){nav.to(getStartPage());},1); };
